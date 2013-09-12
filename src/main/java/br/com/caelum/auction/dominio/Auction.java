df0b27bfe44070eb7e1eaa5befe109d442b58198
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2013-09-12 19:04:45 -0300

    Changed the package name

--- src/main/java/br/com/caelum/auction/dominio/Auction.java
+++ src/main/java/br/com/caelum/auction/dominio/Auction.java
@@ -1,4 +1,4 @@
-package br.com.caelum.leilao.dominio;
+package br.com.caelum.auction.dominio;
 
 import java.util.ArrayList;
 import java.util.Collections;
--- src/main/java/br/com/caelum/auction/dominio/User.java
+++ src/main/java/br/com/caelum/auction/dominio/User.java
@@ -1,4 +1,4 @@
-package br.com.caelum.leilao.dominio;
+package br.com.caelum.auction.dominio;
 
 /**
  * Represents the User.
